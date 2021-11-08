aff66cd4cdccefcd4c11d0d60b71485109f2db18
KenshiTanaka <[1911140064f@ed.fuk.kindai.ac.jp]>
2021-11-08 08:29:20 +0000

    routes変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+ resources :cook
+    root 'cook#index'
+    get 'get_cook/:id', to: 'cook#get_tweet'
+    
+    resources :users
+    get 'get_users/:id', to: 'user#get_user'
+    get 'top/main'
+    post 'top/login'
+    get 'top/logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
