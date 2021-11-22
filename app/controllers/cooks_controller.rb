2ed7045a9509bbdcdc22b133cab69e65c8d7b7db
Ubuntu <ubuntu@ip-172-31-73-147.ec2.internal>
2021-11-22 06:27:52 +0000

    変更した

--- app/controllers/cooks_controller.rb
+++ app/controllers/cooks_controller.rb
@@ -1,26 +1,19 @@
 class CooksController < ApplicationController
   def index
     @cooks = Cook.all
-    def index
-   @cook = Cook.all #SELECT * FROM TWEET;
   end
 
   def new
     @cook = Cook.new
-   @cook = Cook.new
   end
 
   def create
     #ログイン中にしたツイートリンクが表示されないのでsession[:user_id]が空であることは考慮しなくてよい
     user = User.find_by(uid: session[:login_uid])
-    @cook = Cook.new(message: params[:cook][:message], user_id: user.id)
+    @cook = Cook.new(message: params[:tweet][:message], user_id: user.id)
     if @cook.save
       #TODO: ツイートが成功したことをユーザに知らせる
       redirect_to root_path
-   @cook = Cook.new(message: params[:cook][:message], tdate: Time.current)
-    if @cook.save
-     flash[:notice] = '1レコード追加しました'
-     redirect_to root_path
     else
       render 'new'
     end
@@ -30,27 +23,5 @@ class CooksController < ApplicationController
     cook = Cook.find(params[:id])
     cook.destroy
     redirect_to root_path
-  end
-  def show
-   @cook = Cook.find(params[:id])
-  end
-  def destroy
-  cook = Cook.find(params[:id])
-    if cook.destroy
-      flash[:notice] = '1レコード追加しました'
-    end
-    redirect_to root_path
-  end
-  def edit
-    @cook = Cook.find(params[:id])
-  end
-  def update
-    @cook = Cook.find(params[:id])
-    if @cook.update(message: params[:cook][:message])
-      flash[:notice] = '1レコード更新しました'
-      redirect_to root_path
-    else
-      render 'edit'
-    end
   end
 end
