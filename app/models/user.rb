7613a81588ce7c9e4b4e6bf401c16c067f1e4c17
Ubuntu <ubuntu@ip-172-31-73-147.ec2.internal>
2021-11-22 09:06:21 +0000

    変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-    validates :password, presence: true, confirmation: true#裏でチェックする
-  attr_accessor :password,:password_confirmation
+    validates :password, presence: true, confirmation: true
+  
+  attr_accessor :password, :password_confirmation
   
   def password=(val)
     if val.present?
