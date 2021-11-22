1d0619569bf12918dd4e06a1af3efd5c939c070d
Ubuntu <ubuntu@ip-172-31-73-147.ec2.internal>
2021-11-22 06:39:00 +0000

    change

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-    resources :cooks
-    root 'cook#index'
-    get 'get_cook/:id', to: 'cook#get_cook'
-    
-    resources :users
-    get 'get_users/:id', to: 'user#get_user'
-    get 'top/main'
-    post 'top/login'
-    get 'top/logout'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
   resources :users
   resources :cooks
   get 'top/main'
@@ -18,5 +6,4 @@ Rails.application.routes.draw do
   get 'top/logout'
   resources :likes
   root 'cooks#index'
->>>>>>> X
 end
