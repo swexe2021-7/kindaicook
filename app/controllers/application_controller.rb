b031ea7471c3ab987dc440853b4dbb1e4573aea1
KenshiTanaka <[1911140064f@ed.fuk.kindai.ac.jp]>
2021-11-22 08:01:21 +0000

    新規追加完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,11 @@
 class ApplicationController < ActionController::Base
 private
-<<<<<<< HEAD
-=======
 
->>>>>>> 1d0619569bf12918dd4e06a1af3efd5c939c070d
     def current_user
      if session[:login_uid]
       User.find_by(uid: session[:login_uid])
      end
     end
 helper_method :current_user
-<<<<<<< HEAD
-=======
 
->>>>>>> 1d0619569bf12918dd4e06a1af3efd5c939c070d
 end
