4d6b63a029c2f8cb958597e60813cca0ac93d60d
KenshiTanaka <[1911140064f@ed.fuk.kindai.ac.jp]>
2021-11-22 08:57:46 +0000

    main作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
->>>>>>> 11/22
+
   resources :users
   resources :cooks
   get 'top/main'
