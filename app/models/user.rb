63077d815b22b79ac038c58ba6205b1cb6e77250
Ubuntu <ubuntu@ip-172-31-73-147.ec2.internal>
2021-11-29 06:43:46 +0000

    へんんこう

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   
   def password=(val)
     if val.present?
-      self.password = BCrypt::Password.create(val)
+      self.pass= BCrypt::Password.create(val)
     end
     @password = val
   end
